8aa88d16138123e3453fd56db1c991d8479a235a
Jedi0897 <mandar.sohoni@iitb.ac.in>
2016-06-16 16:37:07 +0530

    I think its right, Sagars matches mine

--- ThreeBody.C
+++ ThreeBody.C
@@ -1,16 +1,20 @@
 #define pi 3.141592653589
-#define M 939.5656
-#define m1 938.2723
-#define m2 0.510999
-#define m3 0
-#define n 1000
+#define M 493.7
+#define m1 139.6
+#define m2 139.6
+#define m3 135.0
+#define n 100000
+#define n_bins 150
+
 void ThreeBody(){
+	
+	TH1F* inv_mass=new TH1F("inv_mass","inv_mass",n_bins,75000.,140000);
 	TRandom3* rndgen=new TRandom3();
 	float p1max,p2max;
 	p1max=sqrt((M*M - ((m1+m2+m3)*(m1+m2+m3)))*(M*M - ((-m1+m2+m3)*(-m1+m2+m3))))/(2*M);
 	p2max=sqrt((M*M - ((m1+m2+m3)*(m1+m2+m3)))*(M*M - ((-m2+m1+m3)*(-m2+m1+m3))))/(2*M);
 	float ps_rapid=rndgen->Gaus(0.,3);
-	float pT=0.005*rndgen->Exp(2000.);
+	float pT=0.2+rndgen->Exp(0.5);
 	float theta_mother=2*atan(exp(-1*ps_rapid));
 	float phi_mother=2*pi*rndgen->Uniform(0,1);
 	float p_mother=pT/sin(theta_mother);
@@ -24,6 +28,7 @@ void ThreeBody(){
 	float beta_y=py_mother/E;
 	float beta_z=pz_mother/E;
 	int r = 0;
+	float x[n],y[n];
 	for(int i=0; i<n; i++){
 		float p1cm=rndgen->Uniform(0,p1max);
 		float phi1=2*pi*rndgen->Uniform(0,1);
@@ -60,15 +65,33 @@ void ThreeBody(){
 		float p3z=(gamma-1)*(p3xcm*beta_x*beta_z/(beta*beta) + p3ycm*beta_y*beta_z/(beta*beta) + p3zcm*beta_z*beta_z/(beta*beta)) + p3zcm + gamma*beta_x*E3cm;
 		float E3=gamma*(E3cm + beta_x*p3xcm + beta_y*p3ycm + beta_z*p3zcm);
 		//tests
-		float su=px_mother - (p1x + p2x);
-		float p3=sqrt((p3x*p3x) + (p3y)*(p3y) + (p3z)*(p3z));
-		float p2=sqrt((p2x*p2x) + (p2y)*(p2y) + (p2z)*(p2z));
-		float p1=sqrt((p1x*p1x) + (p1y)*(p1y) + (p1z)*(p1z));
+		float su=px_mother - (p1x + p2x + p3x);
+		float p3=sqrt((p3x*p3x) + (p3y*p3y) + (p3z*p3z));
+		float p2=sqrt((p2x*p2x) + (p2y*p2y) + (p2z*p2z));
+		float p1=sqrt((p1x*p1x) + (p1y*p1y) + (p1z*p1z));
 		float Ediff=E - (E1 + E2 + E3);
-		cout<<Ediff<<endl;
-		if(abs(Ediff) < 0.001){
+		cout<<Ediff<<"  "<<su<<endl;
+		float p12x=p1x+p2x;
+		float p12y=p1y+p2y;
+		float p12z=p1z+p2z;
+		float E12=E1+E2;
+		float p23x=p3x+p2x;
+		float p23y=p3y+p2y;
+		float p23z=p3z+p2z;
+		float E23=E3+E2;
+		if(Ediff*Ediff < 50){
+			x[r]=(E12*E12) - (p12x*p12x) - (p12y*p12y) - (p12z*p12z);
+			y[r]=(E23*E23) - (p23x*p23x) - (p23y*p23y) - (p23z*p23z);
 			r++;
+			inv_mass->Fill(y[r-1]);
 		}
 	}
+	TGraph* dalitz=new TGraph(r,x,y);
+	TCanvas* can=new TCanvas("dalitz","dalitz",1500,700);
+	can->Divide(2,1);
+	can->cd(1);
+	dalitz->Draw("A*");
+	can->cd(2);
+	inv_mass->Draw();
 	cout<<r<<endl;
 }
